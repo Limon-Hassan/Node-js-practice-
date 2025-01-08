0b4f1e3caf15b9b1c47a5d9e1c0e1d299e55d7cb
Mahammud Hassan Limon <mahammudhassanlimon@gmail.com>
2025-01-09 03:01:20 +0600

    data send

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const connection = require("./dbConfig");
 const Taskschema = require("./Model/Taskschema");
 let app = express();
 connection()
-app.post(('/'), async (req, res) => {
-  let task = new Taskschema({
-    name: "Limon"
+app.use(express.json())
+
+app.post('/create', async (req, res) => {
+  let { displayName } = req.body
+
+  let dhoroAmi = new Taskschema({
+    name: displayName
   })
-  let finalresult = await task.save()
-  res.status(201).send(finalresult)
+  await dhoroAmi.save()
+  res.status(201).send("data gese re !")
 })
+
+// app.post(('/'), async (req, res) => {
+//   let task = new Taskschema({
+//     name: "Limon"
+//   })
+//   let finalresult = await task.save()
+//   res.status(201).send(finalresult)
+// })
 
 app.listen(5000, () => {
   console.log("the server is running")
